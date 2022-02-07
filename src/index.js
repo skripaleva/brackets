1034739c994a9e42e99d81daf3a475e3854a1fe4
Evgeniya Skripaleva <evgeniya_ignaten@mail.ru>
2022-02-07 12:28:20 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = {};
+  bracketsConfig.forEach(item => brackets[item[0]] = item[1]);
+  let stack = [];
+  str.split('').forEach(item => {
+    if (brackets[stack[stack.length - 1]] === item) {
+      stack.pop();
+    } else {
+      stack.push(item);
+    }
+  });
+  return !stack.length
+};
